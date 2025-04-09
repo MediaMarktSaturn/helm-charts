53103955413ad5d32d96080bb5bc7a6dcfb48a14
Tung Beier <beiertu@mediamarktsaturn.com>
2025-04-09 23:30:08 +0200

    revert(scripts): revert changes to prep script

--- .github/workflows/scripts/chart-test-prep.sh
+++ .github/workflows/scripts/chart-test-prep.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "[INFO] install flux crds"
+echo "::debug::install flux crds"
 mkdir -p .tmp
 curl -s https://raw.githubusercontent.com/fluxcd/flux2/main/manifests/crds/kustomization.yaml -o .tmp/kustomization.yaml
 kubectl create -k .tmp
 
-echo "[INFO] install istio crds"
+echo "::debug::install istio crds"
 kubectl create -f https://raw.githubusercontent.com/istio/istio/1.22.0/manifests/charts/base/crds/crd-all.gen.yaml
 
-echo "[INFO] install linkerd crds via its cli"
-curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/install | sh
+echo "::debug::install linkerd cli"
+curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/install-edge | sh
 curl --proto '=https' --tlsv1.2 -sSfL https://linkerd.github.io/linkerd-smi/install | sh
 export PATH=$PATH:$HOME/.linkerd2/bin/
+
+echo "::debug::install linkerd crds via its cli"
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.1/standard-install.yaml
 linkerd install --crds | kubectl apply -f -
 
-echo "[INFO] GKE backendconfig crd"
+echo "::debug::GKE backendconfig crd"
 kubectl create -f .github/workflows/scripts/chart-test-prep/backendconfigs.cloud.google.com.yaml
 
-echo "[INFO] install prometheus operator crds"
+echo "::debug::install prometheus operator crds"
+PROMETHEUS_CRDS_URL="https://raw.githubusercontent.com/prometheus-community/helm-charts/main/charts/kube-prometheus-stack/charts/crds/crds"
 kubectl create \
   -f "$PROMETHEUS_CRDS_URL/crd-alertmanagerconfigs.yaml" \
   -f "$PROMETHEUS_CRDS_URL/crd-alertmanagers.yaml" \
@@ -30,11 +33,11 @@ kubectl create \
   -f "$PROMETHEUS_CRDS_URL/crd-servicemonitors.yaml" \
   -f "$PROMETHEUS_CRDS_URL/crd-thanosrulers.yaml"
 
-echo "[INFO] install canary CRD"
+echo "::debug::install canary CRD"
 kubectl create -f https://raw.githubusercontent.com/fluxcd/flagger/main/artifacts/flagger/crd.yaml
 
-echo "[INFO] apply chart preconditions"
+echo "::debug::apply chart preconditions"
 kubectl apply -f .github/workflows/scripts/chart-test-prep/preconditions.yaml
 
-echo "[INFO] install gateway crd"
+echo "::debug::install gateway crd"
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
