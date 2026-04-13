0eec7eea632bd17dbb6be2f00fdd116aa7858f34
Tung Beier <beiertu@mediamarktsaturn.com>
2026-04-13 21:58:38 +0200

    test: print ct-test log in case of error

--- .github/workflows/scripts/chart-test.sh
+++ .github/workflows/scripts/chart-test.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -o pipefail
+
 # copy test-values into charts
 rsync -a chart-tests/ charts/
 
 # run chart tests
-ct install --config .github/workflows/conf/ct-test.yml
+ct install --config .github/workflows/conf/ct-test.yml | tee .tmp/ct-test.log
+
+exit $?
